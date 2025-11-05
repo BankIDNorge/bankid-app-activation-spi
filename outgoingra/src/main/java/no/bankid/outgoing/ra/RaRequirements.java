70945595b3f645575d26b617b8224bfa5c7a14a6
Umair Aamir <umair.aamir@bidbax.no>
2025-11-05 13:34:50 +0100

    Revert version to 1.4.0
    - Keep version at 1.4.0 since it was never officially released
    - Regenerated Swagger documentation with version 1.4.0
    - activation_id field remains in LastVerifiedDTO

--- outgoingra/src/main/java/no/bankid/outgoing/ra/RaRequirements.java
+++ outgoingra/src/main/java/no/bankid/outgoing/ra/RaRequirements.java
@@ -27,7 +27,7 @@ import static no.bankid.outgoing.ra.HttpSignatureHeaders.SIGNATURE;
 @OpenAPIDefinition(
         info = @Info(
                 title = "BankID RA Service Provider Interface (SPI) for activation of BankID App",
-                version = "1.5.0",
+                version = "1.4.0",
                 description = """
                         Defines the interface to be provided by a Registration Authority service to \
                         support activation of BankID App as a HA2 element for an end user's Netcentric BankID.\
